7ef88f8824aed6d002e1816622ce8cad9c049c1b
Ricky <hollowtaylor@gmail.com>
2021-02-07 12:51:47 +0000

    updated movement

--- New Unity Project/Assets/Character/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Character/Scripts/PlayerController.cs
@@ -1,27 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     
     private InputControls _controls;
     private CharacterBase _character;
+    private Rigidbody2D _rb;
 
+    
     [SerializeField]protected float movDir;
     [SerializeField]protected float speed;
     [SerializeField]protected float jumpForce;
     
     [SerializeField] private bool isGrounded;
     [SerializeField] private bool isMoving;
-    [SerializeField] private Vector3 _direction;
+    [SerializeField] private Vector2 _direction;
 
     private void Awake()
     {
+        _rb = GetComponent<Rigidbody2D>();
         _character = GetComponent<CharacterBase>();
         _controls = new InputControls();
         _controls.Enable();  // activate the input system
-
         
+
         /*
          * Started - Key Held
          * Cancelled - Key Released
@@ -30,6 +35,15 @@ public class PlayerController : MonoBehaviour
         _controls.Player.Move.started += OnMoveOnStarted;
         _controls.Player.Move.canceled += OnMoveOnCanceled;
         _controls.Player.Jump.performed += _ => _character.State = CharacterBase.States.Jump;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_character.State == CharacterBase.States.Idle && isGrounded)
+        {
+            _rb.velocity = Vector2.zero;
+            _direction = Vector2.zero;
+        }
     }
 
     private void OnMoveOnCanceled(InputAction.CallbackContext ctx)
@@ -50,29 +64,28 @@ public class PlayerController : MonoBehaviour
 
     public void Move()
     {
-        _direction = new Vector3(movDir * speed * Time.deltaTime, 0);
-        transform.position += _direction;
+        // break if in air
+        if(!isGrounded) return;
+        _direction = Vector2.right * (movDir * speed);
+        _rb.velocity = _direction;
     }
 
     public void Jump()
     {
         // break if in air
         if (!isGrounded) return;
-        
-        // ternary operator, true/false check
-        _direction = !isMoving ? new Vector3(0, 10 * jumpForce * Time.deltaTime) : new Vector3(movDir * speed * Time.deltaTime, 10 * jumpForce * Time.deltaTime);
-        transform.position += _direction;
+        _direction += Vector2.up * jumpForce;
+        _rb.velocity = _direction;
         isGrounded = false;
     }
 
     public void OnCollisionEnter2D(Collision2D other)
     {
-        // ternary operator, true/false check
+        
+        isGrounded = true;
+        _direction = new Vector2(_direction.x, 0);
+        // ternary operator, true(left) / false(right) check
         _character.State = movDir == 0 ? CharacterBase.States.Idle : CharacterBase.States.Moving;
     }
-
-    public void OnCollisionStay2D(Collision2D other)
-    {
-        isGrounded = true;
-    }
+    
 }
